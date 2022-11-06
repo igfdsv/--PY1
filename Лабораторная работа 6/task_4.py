e0a26634642068eb39ae39a84d09f44e25e0ddac
igfdsv <115465384+igfdsv@users.noreply.github.com>
2022-11-06 14:31:05 +0300

    Лабораторная работа 6

---
+++ Лабораторная работа 6/task_4.py
@@ -0,0 +1,21 @@
+import json
+
+INPUT_FILE = "input.csv"
+
+
+def csv_to_list_dict(filename: str,
+                     delimiter: str = ',',
+                     new_line: str = '\n') -> list[dict]:
+    with open(filename, 'r') as input:
+        str_data = input.read()  # not readline to support non-'\n' line delimiter
+    lines = str_data.split(new_line)
+    headers = lines[0].split(delimiter)
+    res = []
+    for l in lines[1:]:
+        values = l.split(delimiter)
+        row = {headers[i]: v for i, v in enumerate(values)}
+        res.append(row)
+    return res
+
+
+print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
